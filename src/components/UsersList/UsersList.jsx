f04fb77a504a1d8b6c28d769b96d5b077ede0f3a
Alterra$Developer <jkiselev2411@gmail.com>
2023-10-08 18:04:34 +0600

    Урок 51. Side Effect

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -2,33 +2,47 @@ import React from "react";
 import s from "./UsersList.module.css";
 import User from "../User/User";
 import axios from "axios";
-import userPhoto from "../../assets/images/user.png"
+import userPhoto from "../../assets/images/user.png";
 
 const UsersList = (props) => {
+  const getUsers = () => {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        const apiUsers = response.data.items;
+        console.log(apiUsers);
+        props.getUsers(
+          apiUsers.map((user) => ({
+            id: user.id,
+            userName: user.name,
+            userIcon: user.photos.small ?? userPhoto,
+            userStatus: user.status,
+            followed: user.followed,
+            // userSurname: "Kiselev",
+            // userLocation: {
+            //   country: "Kyrgyzstan",
+            //   city: "Bishkek",
+            // },
+          }))
+        );
+      });
+  };
 
-  if(props.users.length === 0){
+  const users = props.users.map((u) => (
+    <User
+      key={u.id}
+      user={u}
+      follow={props.follow}
+      unfollow={props.unfollow}
+    ></User>
+  ));
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-      const apiUsers = response.data.items;
-      console.log(apiUsers);
-      props.getUsers(apiUsers.map(user => ({
-        id: user.id,
-        userName: user.name,
-        userIcon: user.photos.small ?? userPhoto,
-        userStatus: user.status,
-        followed: user.followed,
-        // userSurname: "Kiselev",
-        // userLocation: {
-        //   country: "Kyrgyzstan",
-        //   city: "Bishkek",
-        // },
-      })));
-    });
-  }
-
-  const users = props.users.map((u) => <User key={u.id} user={u} follow={props.follow} unfollow={props.unfollow}></User>);
-
-  return <div className={s.users}>{users}</div>;
+  return (
+    <div className={s.users}>
+      <button onClick={getUsers}>Get users</button>
+      <div>{users}</div>
+    </div>
+  );
 };
 
 export default UsersList;
