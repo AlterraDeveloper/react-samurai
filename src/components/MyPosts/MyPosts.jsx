83d4332f37c7925cf075d9bee34e4bc8db80053d
eugene.kiselev <jkiselev2411@gmail.com>
2024-01-11 18:08:47 +0600

    87 - shouldComponentUpdate, PureComponent, memo - React JS

--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -3,22 +3,23 @@ import s from "./MyPosts.module.css";
 import Post from "../Post/Post";
 import MyPostForm from "./MyPostForm";
 
-const MyPosts = (props) => {
-  const postsElements = props.posts.map((p) => (
-    <Post postData={p} key={p.id}></Post>
-  ));
+const MyPosts = React.memo(props => {
 
-  const addPost = (data) => {
-    props.addPost(data.newPostText);
-  };
+    const postsElements = props.posts.map((p) => (
+        <Post postData={p} key={p.id}></Post>
+    ));
 
-  return (
-    <div className="s.postBlock">
-      <h3>My posts</h3>
-      <MyPostForm onSubmit={addPost} />
-      <div className={s.posts}>{postsElements}</div>
-    </div>
-  );
-};
+    const addPost = (data) => {
+        props.addPost(data.newPostText);
+    };
+
+    return (
+        <div className={s.postBlock}>
+            <h3>My posts</h3>
+            <MyPostForm onSubmit={addPost}/>
+            <div className={s.posts}>{postsElements}</div>
+        </div>
+    );
+});
 
 export default MyPosts;
